85988c0afb46db1b11d39716b1590e65ac2d3d32
Michaela Davis <michaela.delight@gmail.com>
2024-09-25 17:16:43 -0700

    :recycle: Update projects section

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,74 @@ return (
     </div>
   </section>
 
+  <section className="section projects">
+    <div className="container">
+      <h2 className="section__header">Projects</h2>
+      <div className="projects__wrapper">
+        <section className='project'>
+            <img src='/assets/screenshots/rocket-mobile.png' className='project__thumbnail'
+              alt='Rocket Mortgage' />
+            <h2 className='project__title'>Rocket Mortgage</h2>
+            <h3 className='project__sub-title'>UX Engineer</h3>
+            <p className='project__description'>
+              Rocket Design System (RDS) engineering team member for the Rocket Family of Companies. RDS serves over 45
+              products within the Rocket Family of Companies, including Rocket Mortgage, Rocket Auto Loans, and Rocket
+              Money. I built and maintained UI components in React and Angular, documented and tested them, and
+              collaborated with design to improve clarity and user experience. I created detailed engineering
+              blueprints, led team discussions on technical paths, and developed a comprehensive video guide for
+              onboarding engineers to the React library of the design system.
+            </p>
+        </section>
+
+        <section className='project'>
+            <img src='/assets/screenshots/puppet-mobile.png' className='project__thumbnail' alt='Puppet homepage' />
+            <h2 className='project__title'>Puppet</h2>
+            <h3 className='project__sub-title'>Senior Web Engineer</h3>
+            <p className='project__description'>
+              Co-maintainer of Puppet.com: JAMstack including a decoupled React frontend with WordPress backend. My
+              responsibilities include web engineering, page building, training, documentation, UI/UX and design
+              implementation, analytics, content authoring support, and interfacing with stakeholders in departments
+              across the company.
+            </p>
+        </section>
+
+        <section className='project'>
+          <img src='/assets/screenshots/mwg-aaa-mobile.png' className='project__thumbnail'
+              alt='AAA Mountain West Group homepage' />
+            <h2 className='project__title'>AAA NCNU</h2>
+            <h3 className='project__sub-title'>Front End Software Engineer</h3>
+            <p className='project__description'>
+              I did things. Co-maintainer of Puppet.com: JAMstack including a decoupled React frontend with WordPress
+              backend. My responsibilities include web engineering, page building, training, documentation, UI/UX and
+              design implementation, analytics, content authoring support, and interfacing with stakeholders in
+              departments across the company.
+            </p>
+
+        </section>
+      </div>
+    </div>
+  </section>
+
+  <section className="section reviews">
+    <div className="container">
+      <h2 className="section__header">Reviews</h2>
+      <div className="reviews__wrapper">
+         <Review 
+          imgURL={'assets/rocket_mortgage_logo.jpeg'} imgAlt={'Rocket Mortgage'} name={'Yashita Reddy'} title={'Director of Engineering, Rocket Mortgage'} review={'... Michaela played a crucial role in fostering collaboration between the design and engineering teams. Her knowledge of design and ability to work cross-functionally helped ensure that all voices were included in the process of building and maintaining the Design System.'}
+        />
+        <Review 
+          imgURL={'assets/elle-hillard.jpeg'} imgAlt={'Elle Hillard'} name={'Elle Hillard'} title={'RDS Engineering Manager, Rocket Mortgage'} review={'Michaela is an outstanding User Experience (UX) Engineer. She possesses a rare combination of attention to detail, problem solving mindset, and such a strong desire to learn and improve... Her positive attitude, enthusiasm, and willingness to go the extra mile have greatly contributed to the positive work environment we have enjoyed in our team.'}
+        />
+        <Review 
+          imgURL={'assets/katie-guernsey.jpeg'} imgAlt={'Katie Guernsey'} name={'Katie Guernsey'} title={'Senior UX Engineer, Rocket Mortgage'} review={'Michaela is incredibly detail oriented and elevated our code review process with that skill. You can always rely on her work to be well thought out and accurate. She is an incredible asset as an engineer and team member. I would highly recommend Michaela to anyone that is looking to elevate their engineering team.'}
+        />
+        <Review 
+          imgURL={'assets/jonathan-waltner.jpeg'} imgAlt={'Jonathan Waltner'} name={'Jonathan Waltner'} title={'Director of Engineering, Airship'} review={' Michaela is a smart, organized, and highly motivated software developer. During her time with us, she developed an excellent working relationship with her teammates, and was able to deliver more functionality than I expected when we started the project. She always came prepared to make sure time was well spent. I could see Michaela being a great addition to almost any team'}
+        />
+      </div>
+    </div>
+  </section>
+
   <section className="section skills">
     <div className="container">
       <h2 className="section__header">Skills</h2>
@@ -54,97 +122,6 @@ return (
           <li className="skills__item">Automation</li>
           <li className="skills__item">Mentorship</li>
         </ul>
-      </div>
-    </div>
-  </section>
-
-  <section className="section projects">
-    <div className="container">
-      <h2 className="section__header">Projects</h2>
-      <div className="projects__wrapper">
-        <section className='project'>
-          <div>
-            <h2 className='project__title'>Rocket Mortgage</h2>
-            <h3 className='project__sub-title'>UX Engineer</h3>
-            <p className='project__description'>
-              Rocket Design System (RDS) engineering team member for the Rocket Family of Companies. RDS serves over 45
-              products within the Rocket Family of Companies, including Rocket Mortgage, Rocket Auto Loans, and Rocket
-              Money. I built and maintained UI components in React and Angular, documented and tested them, and
-              collaborated with design to improve clarity and user experience. I created detailed engineering
-              blueprints, led team discussions on technical paths, and developed a comprehensive video guide for
-              onboarding engineers to the React library of the design system.
-            </p>
-          </div>
-          <div>
-            <img src='/assets/screenshots/rocket.png' className='project__thumbnail'
-              alt='Rocket Mortgage Github #2 Rocket Design System Contributor' />
-          </div>
-        </section>
-
-        <section className='project'>
-          <div>
-            <h2 className='project__title'>Puppet</h2>
-            <h3 className='project__sub-title'>Senior Web Engineer</h3>
-            <p className='project__description'>
-              Co-maintainer of Puppet.com: JAMstack including a decoupled React frontend with WordPress backend. My
-              responsibilities include web engineering, page building, training, documentation, UI/UX and design
-              implementation, analytics, content authoring support, and interfacing with stakeholders in departments
-              across the company.
-            </p>
-          </div>
-          <div>
-            <img src='/assets/screenshots/puppet.png' className='project__thumbnail' alt='Puppet homepage' />
-          </div>
-        </section>
-
-        <section className='project'>
-          <div>
-            <h2 className='project__title'>AAA Northern California, Nevada & Utah</h2>
-            <h3 className='project__sub-title'>Front End Software Engineer</h3>
-            <p className='project__description'>
-              I did things. Co-maintainer of Puppet.com: JAMstack including a decoupled React frontend with WordPress
-              backend. My responsibilities include web engineering, page building, training, documentation, UI/UX and
-              design implementation, analytics, content authoring support, and interfacing with stakeholders in
-              departments across the company.
-            </p>
-          </div>
-          <div>
-            <img src='/assets/screenshots/mwg.aaa.com.png' className='project__thumbnail'
-              alt='AAA Mountain West Group homepage' />
-          </div>
-        </section>
-      </div>
-    </div>
-  </section>
-
-  <section className="section reviews">
-    <div className="container">
-      <h2 className="section__header">Reviews</h2>
-      <div className="reviews__wrapper">
-        {/* <Review 
-          imgURL={'assets/rocket_mortgage_logo.jpeg'} imgAlt={'Rocket Mortgage'} name={'Yashita Reddy'} title={'Director of Engineering, Rocket Mortgage'} review={'I endorse Michaela Davis based on her contributions as a UX Engineer at Rocket. Her dedication to frontend engineering and design systems is evident. Within her role, Michaela demonstrated proficiency of Angular, React, and CSS to deliver features and components for our design system. Michaela played a crucial role in fostering collaboration between the design and engineering teams. Her knowledge of design and ability to work cross-functionally helped ensure that all voices were included in the process of building and maintaining Design System. Michaela\'s collaborative spirit, and desire to grow would make her an invaluable asset to any team. I am confident that she will continue to make significant contributions to any team and would not hesitate to recommend her.'}
-        />
-        <Review 
-          imgURL={'assets/elle-hillard.jpeg'} imgAlt={'Elle Hillard'} name={'Elle Hillard'} title={'RDS Engineering Manager, Rocket Mortgage'} review={'Michaela is an outstanding User Experience (UX) Engineer and has made significant contributions to  our team and the overall success of Rocket. She possesses a rare combination of attention to detail,  problem solving mindset, and such a strong desire to learn and improve that she became a subject  matter expert on the Rocket Design System quickly after she arrived. This is sure to be seen in any  future endeavors she takes on. In addition to her technical capabilities, Michaela is an excellent team player and has an innate ability to  collaborate effectively with colleagues at all levels. Her positive attitude, enthusiasm, and willingness to  go the extra mile have greatly contributed to the positive work environment we have enjoyed in our  team.'}
-        />
-        <Review 
-          imgURL={'assets/katie-guernsey.jpeg'} imgAlt={'Katie Guernsey'} name={'Katie Guernsey'} title={'Senior UX Engineer, Rocket Mortgage'} review={'I worked closely with and mentored Michaela during our time together at Rocket Central. She consistently develops her skills and actively seeks out feedback to grow. She take advantage of learning opportunities, is curious and asks questions. She takes the time to try to understand why we make decisions as a team. She regularly collaborates with other teams to ensure that she is delivering performant and accessible code. Michaela is incredibly detail oriented and elevated our code review process with that skill. You can always rely on her work to be well thought out and accurate. She is an incredible asset as an engineer and team member. I would highly recommend Michaela to anyone that is looking to elevate their engineering team.'}
-        />
-        <Review 
-          imgURL={'assets/jonathan-waltner.jpeg'} imgAlt={'Jonathan Waltner'} name={'Jonathan Waltner'} title={'Director of Engineering, Airship'} review={' Michaela is a smart, organized, and highly motivated software developer. She completed an internship with us in which she was tasked with a largely self-directed project. During her time with us, she developed an excellent working relationship with her teammate and with myself, and was able to deliver more functionality than I expected when we started the project. She always made the most of opportunities to gather feedback and assistance from more experienced developers, and always came prepared with a list of questions to make sure time spent assisting her was well spent. I could see Michaela being a great addition to almost any team'}
-        /> */}
-         <Review 
-          imgURL={'assets/rocket_mortgage_logo.jpeg'} imgAlt={'Rocket Mortgage'} name={'Yashita Reddy'} title={'Director of Engineering, Rocket Mortgage'} review={'... Michaela played a crucial role in fostering collaboration between the design and engineering teams. Her knowledge of design and ability to work cross-functionally helped ensure that all voices were included in the process of building and maintaining the Design System.'}
-        />
-        <Review 
-          imgURL={'assets/elle-hillard.jpeg'} imgAlt={'Elle Hillard'} name={'Elle Hillard'} title={'RDS Engineering Manager, Rocket Mortgage'} review={'Michaela is an outstanding User Experience (UX) Engineer. She possesses a rare combination of attention to detail, problem solving mindset, and such a strong desire to learn and improve... Her positive attitude, enthusiasm, and willingness to go the extra mile have greatly contributed to the positive work environment we have enjoyed in our team.'}
-        />
-        <Review 
-          imgURL={'assets/katie-guernsey.jpeg'} imgAlt={'Katie Guernsey'} name={'Katie Guernsey'} title={'Senior UX Engineer, Rocket Mortgage'} review={'Michaela is incredibly detail oriented and elevated our code review process with that skill. You can always rely on her work to be well thought out and accurate. She is an incredible asset as an engineer and team member. I would highly recommend Michaela to anyone that is looking to elevate their engineering team.'}
-        />
-        <Review 
-          imgURL={'assets/jonathan-waltner.jpeg'} imgAlt={'Jonathan Waltner'} name={'Jonathan Waltner'} title={'Director of Engineering, Airship'} review={' Michaela is a smart, organized, and highly motivated software developer. During her time with us, she developed an excellent working relationship with her teammates, and was able to deliver more functionality than I expected when we started the project. She always came prepared to make sure time was well spent. I could see Michaela being a great addition to almost any team'}
-        />
       </div>
     </div>
   </section>
