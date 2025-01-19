187c57cf541907fe1062cc84c4a881a669e156d6
Michaela Davis <michaela.delight@gmail.com>
2025-01-19 15:25:44 -0800

    Add /puppet

---
+++ src/components/Puppet.tsx
@@ -0,0 +1,8 @@
+import { Work } from "./Work";
+const Puppet = () => {
+  return (
+    <Work imgURL={'assets/screenshots/puppet-landscape.png'} imgAlt={'Puppet homepage landscape'} companyName={'Puppet'} title={'Senior Web Engineer'} work={'I was the co-maintainer of Puppet.com, a JAMstack site consisting of a decoupled custom React front end with a WordPress back end and many integrations, such as Greenhouse and Marketo. My responsibilities include web engineering, page building, training, documentation, UI/UX and design implementation, analytics, content authoring support, and interfacing with stakeholders in departments across the company. I also created engaging animated experiences like the Puppetize timeline and provided training, documentation, and support to streamline the page-building process for content authors.'} scope={'UI/UX, Design System, Engineering'} role={'Senior Web Engineer'} year={'2018-2021'} companyBlurb={'Puppet empowers organizations to streamline and automate their IT infrastructure, enabling faster, more reliable deployments. With its powerful configuration management tools, Puppet simplifies the management of complex systems, ensuring consistency and security across your environments. Whether you’re managing servers, containers, or cloud environments, Puppet helps you scale efficiently while reducing manual effort. Trusted by enterprises worldwide, Puppet delivers the tools you need to innovate faster and stay ahead in today’s dynamic technology landscape.'} img2URL={''} img2Alt={''} skills={"Web development, UI/UX, Design System, Engineering, Mentoring"} stack={"JAMStack (React - Wordpress/PHP), OG: Coupled Drupal"} live={"https://puppet.com"}/>
+  );
+}
+
+export default Puppet;
