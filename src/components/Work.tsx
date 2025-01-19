e998d12b20f66ffb34dc9b498a1a42eca277cdf9
Michaela Davis <michaela.delight@gmail.com>
2025-01-19 15:24:23 -0800

    Add more Work fields

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -3,7 +3,9 @@ import "./work.css";
 
 interface WorkProps {
   imgURL: string;
+  img2URL: string;
   imgAlt: string;
+  img2Alt: string;
   companyName: string;
   title: string;
   work: string;
@@ -11,9 +13,12 @@ interface WorkProps {
   role: string;
   year: string;
   companyBlurb: string;
+  skills: string;
+  stack: string;
+  live: string;
 };
 
-export const Work: React.FC<WorkProps> = ({ imgURL, imgAlt, companyName, title, work, scope, role, year, companyBlurb }) => {
+export const Work: React.FC<WorkProps> = ({ imgURL, img2URL, imgAlt, img2Alt, companyName, title, work, scope, role, year, companyBlurb, skills, stack, live }) => {
   return (
     <section className="section hero">
       <div className="container work">
@@ -28,14 +33,26 @@ export const Work: React.FC<WorkProps> = ({ imgURL, imgAlt, companyName, title, 
               <p>Scope: {scope}</p>
               <p>Role: {role}</p>
               <p>Year: {year}</p>
+              <p>Live: <a href={live}>{live}</a></p>
             </div>
             <div className="work__info-item">
-              <p className="work__body">{companyBlurb}</p>
+              <p className="work__body">About {companyName}: {companyBlurb}</p>
             </div>
           </div>
         </div>
 
-        <p className="work__body">{work}</p>
+        <img className="work__image" src={img2URL} alt={img2Alt} />
+        <div className="work__info-wrapper">
+          <div className="work__info-items">
+            <div className="work__info-item">
+              <p className="work__body">My work at {companyName}: {work}</p>
+            </div>
+            <div className="work__info-item">
+              <p>Skills: {skills}</p>
+              <p>Tech Stack: {stack}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
