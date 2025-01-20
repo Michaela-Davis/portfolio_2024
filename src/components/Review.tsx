a19c6d8c6d20964a9859ef03c223aa65d840f929
Michaela Davis <michaela.delight@gmail.com>
2025-01-19 18:33:07 -0800

    Add links and hover to reviews

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -8,19 +8,22 @@ interface ReviewProps {
   name: string;
   title: string;
   review: string;
+  link: string;
 };
 
-export const Review: React.FC<ReviewProps> = ({ imgURL, imgAlt, name, title, review }) => {
+export const Review: React.FC<ReviewProps> = ({ imgURL, imgAlt, name, title, review, link }) => {
   return (
-    <div className="review">
-      <div className="review__author">
-        <img className="review__image" src={imgURL} alt={imgAlt} />
-        <div className="review__name-title-wrapper">
-          <p className="review__name">{name}</p>
-          <p className="review__title">{title}</p>
+      <div className="review">
+        <div className="review__author">
+          <img className="review__image" src={imgURL} alt={imgAlt} />
+          <div className="review__name-title-wrapper">
+            <p className="review__name">{name}</p>
+            <p className="review__title">{title}</p>
+          </div>
         </div>
+        <a href={link} target="_blank" rel="noreferrer" className="review__link">
+          <p className="review__body">{review}</p>
+        </a>
       </div>
-      <p className="review__body">{review}</p>
-    </div>
   );
 }
