b83f26dd7633b50111206a22d8778217541b5d17
Michaela Davis <michaela.delight@gmail.com>
2025-01-20 11:14:15 -0800

    :lipstick: grayText utility

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./work.css";
 
 interface WorkProps {
@@ -29,13 +28,13 @@ export const Work: React.FC<WorkProps> = ({ imgURL, img2URL, imgAlt, img2Alt, co
         <div className="work__info-wrapper">
           <div className="work__info-items">
             <div className="work__info-item">
-              <p>Scope: {scope}</p>
-              <p>Role: {role}</p>
-              <p>Year: {year}</p>
-              <p>Live: <a href={live}>{live}</a></p>
+              <p><span className="grayText">Scope:</span>  {scope}</p>
+              <p><span className="grayText">Role:</span> {role}</p>
+              <p><span className="grayText">Year:</span>Year: {year}</p>
+              <p><span className="grayText">Live:</span> <a href={live}>{live}</a></p>
             </div>
             <div className="work__info-item">
-              <p className="work__body">About {companyName}: {companyBlurb}</p>
+              <p className="work__body"><span className="grayText">About {companyName}:</span> {companyBlurb}</p>
             </div>
           </div>
         </div>
@@ -44,11 +43,11 @@ export const Work: React.FC<WorkProps> = ({ imgURL, img2URL, imgAlt, img2Alt, co
         <div className="work__info-wrapper">
           <div className="work__info-items">
             <div className="work__info-item">
-              <p className="work__body">My work at {companyName}: {work}</p>
+              <p className="work__body"><span className="grayText">My work at {companyName}:</span> {work}</p>
             </div>
             <div className="work__info-item">
-              <p>Skills: {skills}</p>
-              <p>Tech Stack: {stack}</p>
+              <p><span className="grayText">Skills:</span> {skills}</p>
+              <p><span className="grayText">Tech Stack:</span> {stack}</p>
             </div>
           </div>
         </div>
